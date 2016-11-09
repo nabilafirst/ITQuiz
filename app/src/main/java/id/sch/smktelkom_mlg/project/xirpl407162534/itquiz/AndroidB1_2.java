3575a89d9c994bf35906b4fa3b29d8a2c888cfb8
Nabilafirst <nabilafirsta@gmail.com>
2016-11-09 07:14:25 +0700

    Soal Android Basic 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/AndroidB1_2.java
@@ -0,0 +1,24 @@
+package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AndroidB1_2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_b1_2);
+
+        findViewById(R.id.imageViewNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AndroidB1_2.this, AndroidB1_3.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/AndroidB1_5.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidB1_5 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_b1_5);
+    }
+}

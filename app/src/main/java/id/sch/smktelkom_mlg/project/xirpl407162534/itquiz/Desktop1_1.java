5b11c940ba91c2e844ca0a2c0a02cd3260f3818b
Yoan Kresna <kresnajr213@gmail.com>
2016-11-12 21:10:11 +0700

    Soal Desktop 1_1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/Desktop1_1.java
@@ -0,0 +1,118 @@
+package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Desktop1_1 extends Activity implements View.OnTouchListener, View.OnClickListener {
+
+    Button btnA, btnB, btnC, btnD;
+    ImageView alert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desktop1_1);
+
+
+        initButton();
+
+        initButtonClickedCallback();
+
+
+        alert = (ImageView) findViewById(R.id.imageViewCek);
+        alert.setOnClickListener(this);
+        btnA = (Button) findViewById(R.id.buttonA);
+        btnA.setOnClickListener(this);
+        btnB = (Button) findViewById(R.id.buttonB);
+        btnB.setOnClickListener(this);
+        btnC = (Button) findViewById(R.id.buttonC);
+        btnC.setOnClickListener(this);
+        btnD = (Button) findViewById(R.id.buttonD);
+        btnD.setOnClickListener(this);
+
+
+        findViewById(R.id.imageViewNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Desktop1_1.this, Desktop1_2.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == alert) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Penjelasan")
+                    .setMessage("Desktop adalah aplikasi yang digunakan pada perangkat laptop atau komputer," +
+                            " diciptakan oleh Bell labs, pada tahun 1980-an")
+                    .setNeutralButton("TUTUP", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dlg, int sumthin) {
+
+                        }
+                    })
+                    .show();
+        } else if (view == btnD) {
+            Toast.makeText(this, "Jawaban anda benar", Toast.LENGTH_SHORT).show();
+        } else if (view == btnA) {
+            Toast.makeText(this, "Jawaban anda salah", Toast.LENGTH_SHORT).show();
+        } else if (view == btnB) {
+            Toast.makeText(this, "Jawaban anda salah", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Jawaban anda salah", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initButtonClickedCallback() {
+        btnA.setOnTouchListener(this);
+        btnB.setOnTouchListener(this);
+        btnC.setOnTouchListener(this);
+        btnD.setOnTouchListener(this);
+    }
+
+    private void initButton() {
+        btnA = (Button) findViewById(R.id.buttonA);
+        btnB = (Button) findViewById(R.id.buttonB);
+        btnC = (Button) findViewById(R.id.buttonC);
+        btnD = (Button) findViewById(R.id.buttonD);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent motionEvent) {
+
+        switch (v.getId()) {
+            case R.id.buttonA:
+                btnA.setBackgroundColor(Color.RED);
+                break;
+            case R.id.buttonB:
+                btnB.setBackgroundColor(Color.RED);
+                break;
+            case R.id.buttonC:
+                btnC.setBackgroundColor(Color.RED);
+                break;
+            case R.id.buttonD:
+                btnD.setBackgroundColor(Color.GREEN);
+                break;
+        }
+        btnA.setPressed(btnA.isPressed());
+        btnB.setPressed(btnB.isPressed());
+        btnC.setPressed(btnC.isPressed());
+        btnD.setPressed(btnD.isPressed());
+        return false;
+
+    }
+}

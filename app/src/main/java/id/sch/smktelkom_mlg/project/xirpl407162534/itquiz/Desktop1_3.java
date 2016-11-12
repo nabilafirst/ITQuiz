d8b0bf06cd7fdc8ef50487e9d457209c9ff8cfcb
Yoan Kresna <kresnajr213@gmail.com>
2016-11-12 21:12:47 +0700

    Soal Desktop 1_2

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/Desktop1_3.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Desktop1_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desktop1_3);
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/DesktopHome.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/DesktopHome.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DesktopHome extends Activity {
 
@@ -9,5 +11,33 @@ public class DesktopHome extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desktop_home);
+
+        findViewById(R.id.imageViewBasic1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DesktopHome.this, Desktop1_1.class);
+                startActivity(intent);
+
+            }
+        });
+
+        findViewById(R.id.imageViewBasic2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DesktopHome.this, AndroidB2_1.class);
+                startActivity(intent);
+
+            }
+        });
+
+        findViewById(R.id.imageViewBasic3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DesktopHome.this, AndroidB3_1.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }

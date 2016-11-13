e1d30a7b24164e032f9d79ec7ea644a37353b940
Lenovo <frida_baharizki_24rpl@student.smktelkom-mlg.sch.id>
2016-11-13 10:33:24 +0700

    Java Basic 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/JavaB1_1.java
@@ -0,0 +1,52 @@
+package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.ImageView;
+
+public class JavaB1_1 extends Activity implements View.OnClickListener {
+
+    ImageView alert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java_b1_1);
+
+        alert = (ImageView) findViewById(R.id.imageViewCek);
+        alert.setOnClickListener(this);
+
+        findViewById(R.id.imageViewNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JavaB1_1.this, JavaB1_2.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == alert) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Penjelasan")
+                    .setMessage("Javac singkatan dari Java Compile, dimana compile artinya menyusun." +
+                            " Berarti saat kita melakukan compile, saat itu berkas Java disusun agar" +
+                            " dapat dijalankan.")
+                    .setNeutralButton("TUTUP", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dlg, int sumthin) {
+
+                        }
+                    })
+                    .show();
+        }
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/JavaHome.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl407162534/itquiz/JavaHome.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl407162534.itquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class JavaHome extends Activity {
 
@@ -9,5 +11,14 @@ public class JavaHome extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_home);
+
+        findViewById(R.id.imageViewBasic1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JavaHome.this, JavaB1_1.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
